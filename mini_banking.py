ca186215671cac3403991ed7ebfc090b6b15fecc
Hamshavarsan <kanga8482@gmail.com>
2025-05-10 19:44:29 +0530

    Update mini_banking.py

--- mini_banking.py
+++ mini_banking.py
@@ -1,25 +1,34 @@
 from datetime import datetime
 
-
 accounts = {}
 next_account_number = 1001
 
-def save_data_to_file():
-    with open("bank_data.txt", "w") as file:
+def user_data():
+    with open("user_data.txt", "w") as file:
         for acc_no, info in accounts.items():
             file.write(f"Account Number: {acc_no}\t")
             file.write(f"Holder: {info['name']}\t")
+            file.write(f"Holder: {info['email']}\t")
+            file.write(f"Holder: {info['phone']}\t")
             file.write(f"Balance: {info['balance']}\t")
             file.write("Transactions:\t")
             for transaction in info['transactions']:
                 file.write(f"{transaction}\t")
             file.write("\n")
-    
+
 
 def create_account():
     global next_account_number
 
     name = input("Enter account holder name: ").strip()
+    email= input("Enter account holder email:").strip()
+    try:
+        phone= int (input("Enter account holder phone number:").strip())
+    except ValueError:
+        print("Invalid phone number. Please enter phone number.")
+
+
+    
     try:
         initial_balance = float(input("Enter initial balance: "))
         if initial_balance < 0:
@@ -33,12 +42,14 @@ def create_account():
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     accounts[account_number] = {
         'name': name,
+        'email':email,
+        'phone':phone,
         'balance': initial_balance,
         'transactions': [f"[{timestamp}] Initial deposit: {initial_balance}"]
     }
     print(f"Account created successfully! Your account number is {account_number}")
     next_account_number += 1
-    save_data_to_file()
+    user_data()
 
 def deposit_money(account_number):
     try:
@@ -85,69 +96,66 @@ def transaction_history(account_number):
         print(f"- {transaction}")
 
 
-def save_data_to_file():
-    with open("bank_data.txt", "a") as file:
-        for acc_no, info in accounts.items():
-            file.write(f"Account Number: {acc_no}\t")
-            file.write(f"Holder: {info['name']}\t")
-            file.write(f"Balance: {info['balance']}\t")
-            file.write("Transactions:\t")
-            for transaction in info['transactions']:
-                file.write(f"  {transaction}\t")
-            file.write("\n")
-    print("Data saved to 'bank_data.txt'")
 
 def admin_menu():
     while True:
         print("Admin Menu:")
         print("1. Create Account")
         print("2. View All Accounts")
-        #print("3. Save Data to File")
         print("3. Logout")
 
         choice = input("Choose option (1-3): ").strip()
         if choice == '1':
             create_account()
         elif choice == '2':
-            for acc_no, info in accounts.items():
-                print(f"\nAccount Number: {acc_no}")
-                print(f"Holder: {info['name']}")
-                print(f"Balance: {info['balance']}")
-                print("Transactions:")
-                for t in info['transactions']:
-                    print(f"  {t}")
-                # elif choice == '3':
-                #     save_data_to_file()
+            with open("user_data.txt","r") as file:
+                print(file.read())   
         elif choice == '3':
             break
         else:
             print("Invalid choice.")
 
 
-def user_menu(account_number):
-    while True:
-        print(f"Welcome {accounts[account_number]['name']}")
-        print("1. Deposit Money")
-        print("2. Withdraw Money")
-        print("3. Check Balance")
-        print("4. Transaction History")
-        print("5. Logout")
 
-        choice = input("Choose option (1-5): ").strip()
+def  user_menu():
+    acc_no = input("Enter your account number: ").strip()
+    if acc_no in accounts:
+        account_number=acc_no
+        while True:
+            print(f"Welcome {accounts[account_number]['name']}")
+            print("1. Deposit Money")
+            print("2. Withdraw Money")
+            print("3. Check Balance")
+            print("4. Transaction History")
+            print("5. Logout")
 
-        if choice == '1':
-            deposit_money(account_number)
-        elif choice == '2':
-            withdraw_money(account_number)
-        elif choice == '3':
-            check_balance(account_number)
-        elif choice == '4':
-            transaction_history(account_number)
-        elif choice == '5':
-            break
-        else:
-            print("Invalid choice.")
-
+            choice = input("Choose option (1-5): ").strip()
+            if choice == '1':
+                acc_no = input("Enter your account number: ").strip()
+                if acc_no in accounts:
+                    account_number=acc_no
+                    deposit_money(account_number)
+            elif choice == '2':
+                acc_no = input("Enter your account number: ").strip()
+                if acc_no in accounts:
+                    account_number=acc_no
+                    withdraw_money(account_number)
+            elif choice == '3':
+                acc_no = input("Enter your account number: ").strip()
+                if acc_no in accounts:
+                    account_number=acc_no
+                    check_balance(account_number)
+            elif choice == '4':
+                acc_no = input("Enter your account number: ").strip()
+                if acc_no in accounts:
+                    account_number=acc_no
+                    transaction_history(account_number)
+            elif choice == '5':
+                break
+            
+            
+    else:
+        print("Account not found.")
 def login():
     while True:
         print("Login Menu:")
@@ -164,11 +172,7 @@ def login():
             else:
                 print("Incorrect password.")
         elif choice == '2':
-            acc_no = input("Enter your account number: ").strip()
-            if acc_no in accounts:
-                user_menu(acc_no)
-            else:
-                print("Account not found.")
+           user_menu()
         elif choice == '3':
             print("Thank you for using the banking app😊")
             break
@@ -176,5 +180,9 @@ def login():
             print("Invalid choice.")
 
 login()
+
+
+
+
 
 
